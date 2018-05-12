460e757186245d66343b36fc7abd3b145743c389
Arpit Roopchandani <arpit9295@gmail.com>
2018-05-12 18:17:20 +0530

    functions completed, data json changed

--- app/test-data.js
+++ app/test-data.js
@@ -1,56 +1,30 @@
-const data = {
-  "staff" : [
-    {
-      "name": "Andrews Simon",
-      "availability": [
-        [1525932000000, 1525946400000],
-        [1525953600000, 1525975200000],
-      ],
-      "bookings": [{
-        "status": "start",
-        "timeSlot": [1525939200000, 1525939800000],
-      }, {
-        "status": "booked",
-        "timeSlot": [1525939800000, 1525946400000],
-      }],
-    }
-  ],
-  "status": {
-    "free": {
-      "color": "#ffffff",
-      "name": "Free",
-    },
-    "start": {
-      "color": "#ffffaa",
-      "name": "Start",
-    },
-    "booked": {
-      "color": "#ffaaff",
-      "name": "Booked",
-    },
-    "clash": {
-      "color": "#aaaaff",
-      "name": "Clash",
-    },
-    "available": {
-      "color": "#ffaaaa",
-      "name": "Available",
-    },
-    "holiday": {
-      "color": "#aaffaa",
-      "name": "Holiday",
-    },
-    "sick": {
-      "color": "#aaffcc",
-      "name": "Sick",
-    },
-    "training": {
-      "color": "#ccffaa",
-      "name": "Training",
-    },
-    "other": {
-      "color": "#ffccaa",
-      "name": "Other",
-    },
+const data = [
+  {
+    "name": "Andrews Simon",
+    "availability": [
+      [1525932000000, 1525946400000],
+      [1525953600000, 1525975200000],
+    ],
+    "bookings": [{
+      "status": "start",
+      "timeSlot": [1525939200000, 1525939800000],
+    }, {
+      "status": "booked",
+      "timeSlot": [1525939800000, 1525946400000],
+    }],
+  },
+  {
+    "name": "Andrews Simon",
+    "availability": [
+      [1525932000000, 1525946400000],
+      [1525953600000, 1525975200000],
+    ],
+    "bookings": [{
+      "status": "start",
+      "timeSlot": [1525939200000, 1525939800000],
+    }, {
+      "status": "booked",
+      "timeSlot": [1525939800000, 1525946400000],
+    }],
   }
-};
+];
